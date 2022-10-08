370b9a35dca1936bf12c3faf7329f3127c54b4e6
Gabriel de Araújo <110828944+Nikt27@users.noreply.github.com>
2022-10-07 22:40:17 -0300

    Add files via upload

---
+++ senCosTan.py
@@ -0,0 +1,5 @@
+from math import sin,cos,tan
+num = int(input('Digite um angulo: '))
+print (f'Seno: {round(sin(num),2)}')
+print (f'Cosseno: {round(cos(num),2)}')
+print (f'Tangente: {round(tan(num),2)}')
