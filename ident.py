d0e9f28b632648f709df224b83bd10a373967c4a
Gabriel de Araújo <110828944+Nikt27@users.noreply.github.com>
2022-10-07 22:21:05 -0300

    Add files via upload
    Um código para identificar se um caractere é uma letra, número e/ou alfanumérico

---
+++ ident.py
@@ -0,0 +1,4 @@
+n=input('Digite algo: ')
+print('O que você digitou é um número? ', n.isnumeric())
+print('O que você digitou é uma letra? ', n.isalpha())
+print('O que você digitou é alfanumérico? ', n.isalnum())
