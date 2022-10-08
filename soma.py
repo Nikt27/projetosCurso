3745dc616b2e1f6335c053c9682d79debc6313d1
Gabriel de Araújo <110828944+Nikt27@users.noreply.github.com>
2022-10-07 22:41:47 -0300

    Add files via upload

---
+++ soma.py
@@ -0,0 +1,6 @@
+n1=int(input('Digite o primeiro número: '))
+n2=int(input('Digite o segundo número: '))
+s=n1+n2
+#print('O resultado da soma entre', n1 , 'e' , n2 ,'é', s)
+#print('O resultado da soma entre {} e {} é {}'.format(n1, n2, s))
+print(f'O resultado da soma entre {n1} e {n2} é {s}')
